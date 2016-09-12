35109a01037f61c724392bd08b4d910f3628ca90
hpcsc <hungphuongjc@gmail.com>
2016-09-13 00:03:52 +0800

    change Initialize() to internal, move private invoke method to extension class

--- Sharpenter.BootstrapperLoader/BootstrapperLoader.cs
+++ Sharpenter.BootstrapperLoader/BootstrapperLoader.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Microsoft.Practices.ServiceLocation;
+using Sharpenter.BootstrapperLoader.Helpers;
 using Sharpenter.BootstrapperLoader.Internal;
 
 namespace Sharpenter.BootstrapperLoader
@@ -17,7 +17,7 @@ namespace Sharpenter.BootstrapperLoader
             _config = config;
         }
 
-        public void Initialize()
+        internal void Initialize()
         {
             var assemblies = _config.AssemblyProvider.Find();
 
@@ -37,8 +37,8 @@ namespace Sharpenter.BootstrapperLoader
             foreach (var bootstrapper in _bootstrappers)
             {
                 bootstrapper.GetType()
-                    .GetMethod(_config.ConfigureContainerMethodName, new [] { container.GetType() })?
-                    .Invoke(bootstrapper, new[] { container });
+                    .GetMethod(_config.ConfigureContainerMethodName, new [] { container.GetType() })
+                    ?.Invoke(bootstrapper, new[] { container });
             }
         }
 
@@ -46,46 +46,11 @@ namespace Sharpenter.BootstrapperLoader
         {
             foreach (var bootstrapper in _bootstrappers)
             {
-                InvokeMethodIfAvailable(bootstrapper, _config.ConfigureMethodName, serviceLocator);
+                bootstrapper
+                    .GetType()
+                    .GetMethod(_config.ConfigureMethodName)
+                    ?.InvokeWithDynamicallyResolvedParameters(bootstrapper, serviceLocator);                
             }
-        }      
-
-        private static void InvokeMethodIfAvailable(object bootstrapper, string methodName, IServiceLocator serviceLocator)
-        {
-            var configureMethod = bootstrapper.GetType().GetMethod(methodName);
-            if (configureMethod != null)
-            {
-                InvokeMethodWithDynamicallyResolvedParameters(bootstrapper, configureMethod, serviceLocator);
-            }
-        }
-
-        private static void InvokeMethodWithDynamicallyResolvedParameters(object bootstrapper, MethodInfo configureMethod, IServiceLocator serviceLocator)
-        {
-            var parameterInfos = configureMethod.GetParameters();
-            var parameters = new object[parameterInfos.Length];
-            for (var index = 0; index < parameterInfos.Length; index++)
-            {
-                var parameterInfo = parameterInfos[index];
-                if (parameterInfo.ParameterType == typeof(IServiceLocator))
-                {
-                    parameters[index] = serviceLocator;
-                }
-                else
-                {
-                    try
-                    {
-                        parameters[index] = serviceLocator.GetInstance(parameterInfo.ParameterType);
-                    }
-                    catch (Exception ex)
-                    {
-                        throw new Exception(
-                            $"Could not resolve a service of type '{parameterInfo.ParameterType.FullName}' for the parameter '{parameterInfo.Name}' of method '{configureMethod.Name}' on type '{configureMethod.DeclaringType.FullName}'.",
-                            ex);
-                    }
-                }
-            }
-
-            configureMethod.Invoke(bootstrapper, parameters);
         }
     }
 }
---
+++ Sharpenter.BootstrapperLoader/Helpers/MethodInfoExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Sharpenter.BootstrapperLoader.Helpers
+{
+    public static class MethodInfoExtensions
+    {
+        public static void InvokeWithDynamicallyResolvedParameters(this MethodInfo configureMethod, object bootstrapper, IServiceLocator serviceLocator)
+        {
+            var parameterInfos = configureMethod.GetParameters();
+            var parameters = new object[parameterInfos.Length];
+            for (var index = 0; index < parameterInfos.Length; index++)
+            {
+                var parameterInfo = parameterInfos[index];
+                if (parameterInfo.ParameterType == typeof(IServiceLocator))
+                {
+                    parameters[index] = serviceLocator;
+                }
+                else
+                {
+                    try
+                    {
+                        parameters[index] = serviceLocator.GetInstance(parameterInfo.ParameterType);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(
+                            $"Could not resolve a service of type '{parameterInfo.ParameterType.FullName}' for the parameter '{parameterInfo.Name}' of method '{configureMethod.Name}' on type '{configureMethod.DeclaringType.FullName}'.",
+                            ex);
+                    }
+                }
+            }
+
+            configureMethod.Invoke(bootstrapper, parameters);
+        }
+    }
+}
