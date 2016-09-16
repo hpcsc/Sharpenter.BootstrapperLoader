ffaabef5cef8345e6e971ad454a8241457afadac
hpcsc <hungphuongjc@gmail.com>
2016-09-16 22:14:06 +0800

    add packaging script

---
+++ deployment/package.ps1
@@ -0,0 +1,19 @@
+cd ../
+
+Write-Host "==============================================="
+Write-Host "==========      Building   ===================="
+
+& ".\build.ps1"
+
+Write-Host "==============================================="
+Write-Host "==========      Packaging   ==================="
+
+Copy-Item ".\deploy\Sharpenter.BootstrapperLoader.nuspec" -Destination ".\Sharpenter.BootstrapperLoader"
+
+cd ./Sharpenter.BootstrapperLoader
+
+Invoke-Expression "..\tools\nuget.exe pack Sharpenter.BootstrapperLoader.csproj -Prop Configuration=Release"
+
+Remove-Item ".\Sharpenter.BootstrapperLoader.nuspec"
+
+cd ../deploy
